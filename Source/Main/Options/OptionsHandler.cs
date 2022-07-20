0c712d240665d104d2beadc4a5265cecd1792b6a
Vertic <thisistherealequinoctic@gmail.com>
2022-07-20 23:13:11 +0800

    New Options System :eyes:

--- Source/Main/Options/OptionsHandler.cs
+++ Source/Main/Options/OptionsHandler.cs
@@ -7,6 +7,11 @@ namespace Revenge.Source.Main.Options
 {
 	public class OptionsHandler
 	{
+		string[] testOptionsArr = {
+			"One",
+			"Two",
+			"Three"
+		};
 
 		public void CreateOptions() {
 			Option option = new Option();
@@ -16,9 +21,21 @@ namespace Revenge.Source.Main.Options
 			/* Test Category */
 			textDivider.CreateDivider("-", 120, true);
 			category.CreateCategory("Test Category", true, true, true, 6);
-			option.CreateOption(1, "Test Option 1", true, true, 6);
-			option.CreateOption(2, "Test Option 1", true, true, 6);
-			option.CreateOption(3, "Test Option 1", true, true, 6);
+			for (int i = 1; i <= 3; i++) {
+				option.CreateOption(i, "Test Option " + i, true, true, 6);
+			}
+
+			/* Test Array Category */
+			textDivider.CreateDivider("-", 120, true);
+			category.CreateCategory("Test Arrays", true, true, true, 6);
+			GetOptions(testOptionsArr, testOptionsArr.Length, 4);
+		}
+
+		void GetOptions(string[] arr, int length, int increment) {
+			Option option = new Option();
+			for (int i = 0; i < length; i++) {
+				option.CreateOption(i+increment, arr[i]);
+			}
 		}
 	}
 }
