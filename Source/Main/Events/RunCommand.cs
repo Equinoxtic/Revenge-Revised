16a9e0ca2a91469804f5b965cf92dc3477b43d20
Vertic <thisistherealequinoctic@gmail.com>
2022-07-21 01:19:33 +0800

    Add new commands

--- Source/Main/Events/RunCommand.cs
+++ Source/Main/Events/RunCommand.cs
@@ -1,11 +1,26 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Revenge.Source.Main.RevLib;
 
 namespace Revenge.Source.Main.Events
 {
 	public class RunCommand
 	{
-		// TODO: Finish Code.
+		public void RunCommandEvent(string? command, bool returnToMain = true) {
+			Program program = new Program();
+			ErrorParser errorParser = new ErrorParser();
+			ClientExit clientExit = new ClientExit();
+			if (!String.IsNullOrEmpty(command)) {
+				switch(command) {
+					case "--exit": clientExit.InitExit(true); break;
+				}
+			} else {
+				errorParser.ErrorSwitch("nul", "String");
+			}
+			if (returnToMain) {
+				program.ReInit(false);
+			}
+		}
 	}
 }
--- Source/Main/Events/RunOption.cs
+++ Source/Main/Events/RunOption.cs
@@ -1,11 +1,25 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Revenge.Source.Main.RevLib;
 
 namespace Revenge.Source.Main.Events
 {
 	public class RunOption
 	{
-		// TODO: Finish Code.
+		public void RunOptionEvent(int option, bool returnToMain = true) {
+			Program program = new Program();
+			ErrorParser errorParser = new ErrorParser();
+			if (option != 0) {
+				switch(option) {
+					case 1: Console.Write("\njriviwjiiorg\n"); break;
+				}
+			} else {
+				errorParser.ErrorSwitch("inv", "Number", Convert.ToString(option));
+			}
+			if (returnToMain) {
+				program.ReInit(false);
+			}
+		}
 	}
 }
