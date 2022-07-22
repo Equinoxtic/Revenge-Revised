6d7322b7a06936399483a81ef7949c7000562a07
Vertic <thisistherealequinoctic@gmail.com>
2022-07-23 03:17:26 +0800

    Create ModuleInit.cs

---
+++ Source/Main/Init/ModuleInit.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Revenge.Modules;
+
+namespace Revenge.Source.Main.Init
+{
+	public class ModuleInit
+	{
+		public void RunClientModules()
+		{
+			/* =====================
+			* Run Module Code here *
+			===================== */
+		}
+
+		public void GetModuleProperties()
+		{
+			/* ===========================
+			* Get Module Properties here *
+			=========================== */
+		}
+	}
+}
