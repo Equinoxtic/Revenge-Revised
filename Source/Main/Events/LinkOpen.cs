96eccee93bb1d1fe67cccb9be55cfec68b7becdb
Vertic <thisistherealequinoctic@gmail.com>
2022-07-25 15:31:48 +0800

    Create LinkOpen.cs

---
+++ Source/Main/Events/LinkOpen.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Revenge.Source.Main.RevLib;
+
+namespace Revenge.Source.Main.Events
+{
+	public class LinkOpen
+	{
+		public void InitLink(string? _link) {
+			Link link = new Link();
+			if (!String.IsNullOrEmpty(_link)) {
+				link.OpenLink(_link);
+			}
+ 		}
+	}
+}
