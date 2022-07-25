9751d7733e03322c628331c330e54ad59a40cbe9
Vertic <thisistherealequinoctic@gmail.com>
2022-07-25 22:28:53 +0800

    Update Listener.cs

--- Source/Main/RevLib/Listener.cs
+++ Source/Main/RevLib/Listener.cs
@@ -5,16 +5,16 @@ using Revenge.Source.Main.Events;
 
 namespace Revenge.Source.Main.RevLib
 {
-	public class InputCommand
+	public class Listener
 	{
 		public void ListenForCommandInput(string? _input) {
 			RunCommand runCommand = new RunCommand();
 			RunOption runOption = new RunOption();
-			Revenge.Program = new Revenge.Program();
+			Revenge.Program program = new Revenge.Program();
 			if (!String.IsNullOrEmpty(_input)) {
 				if (_input.StartsWith("-") || _input.StartsWith("--")) {
 					runCommand.RunCommandEvent(_input);
-				} else { runOption.RunOptionEvent(Convert.ToInt32(_input)); break; }
+				} else { runOption.RunOptionEvent(Convert.ToInt32(_input)); }
 			} else {
 				program.ReInit(false);
 			}
