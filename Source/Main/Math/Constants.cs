732b06c26d6d8f839ee18addc9abc4bf63c2cab8
Vertic <thisistherealequinoctic@gmail.com>
2022-07-27 17:36:17 +0800

    Add Constants + Updates

---
+++ Source/Main/Math/Constants.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Revenge.Source.Main.Math
+{
+	public class Constants
+	{
+		public double PI() {
+			return System.Math.PI;
+		}
+
+		public double E() {
+			return System.Math.E;
+		}
+	}
+}
--- Source/Main/Math/FkMath.cs
+++ Source/Main/Math/FkMath.cs
@@ -8,11 +8,15 @@ namespace Revenge.Source.Main.Math
 	public class FkMath
 	{
 		Functions functions = new Functions();
+		Trigonometric trigonometric = new Trigonometric();
+		Constants constants = new Constants();
 		ErrorParser errorParser = new ErrorParser();
 
-		public void Operation(string? type, int a, int b) {
+		public void Operation(string? type, int a = 0, int b = 0) {
 			float a_fl = (float)a;
 			float b_fl = (float)b;
+			double a_db = (double)a;
+			double b_db = (double)b;
 			if (!String.IsNullOrEmpty(type)) {
 				switch(type) {
 					case "+" or "#add" or "Add":
@@ -27,38 +31,68 @@ namespace Revenge.Source.Main.Math
 					case "/" or"#div" or "Divide":
 						Console.Write(Divide(a, b));
 						break;
+					case "sin" or "sine" or "Sine":
+						Console.Write(Sine(a));
+						break;
+					case "cos" or "cosine" or "Cosine":
+						Console.Write(Cosine(a));
+						break;
+					case "tan" or "tangent" or "Tangent":
+						Console.Write(Tangent(a));
+						break;
+					case "pi" or "PI":
+						Console.Write(constants.PI());
+						break;
+					case "e" or "E":
+						Console.Write(constants.E());
+						break;
 				}
 			} else {
 				errorParser.ErrorSwitch("nul", "Operation");
 			}
 		}
 
-		public int Add(int addend_a, int addend_b) {
+		int Add(int addend_a, int addend_b) {
 			int sum = functions.IntAdd(
 				addend_a, addend_b
 			);
 			return sum;
 		}
 
-		public int Subtract(int minuend, int subtrahend) {
+		int Subtract(int minuend, int subtrahend) {
 			int difference = functions.IntSub(
 				minuend, subtrahend
 			);
 			return difference;
 		}
 
-		public int Multiply(int multiplicand, int multiplier) {
+		int Multiply(int multiplicand, int multiplier) {
 			int product = functions.IntMult(
 				multiplicand, multiplier
 			);
 			return product;
 		}
 
-		public float Divide(float dividend, float divisor) {
+		float Divide(float dividend, float divisor) {
 			float quotient = functions.IntDiv(
 				dividend, divisor
 			);
 			return quotient;
+		}
+
+		double Sine(double a) {
+			double n_sin = trigonometric.Sin(a);
+			return (n_sin > 0) ? n_sin : 0;
+		}
+
+		double Cosine(double b) {
+			double n_cos = trigonometric.Cos(b);
+			return (n_cos > 0) ? n_cos : 0;
+		}
+
+		double Tangent(double c) {
+			double n_tan = trigonometric.Tan(c);
+			return (n_tan > 0) ? n_tan : 0;
 		}
 	}
 }
