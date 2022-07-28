5bd0812587383e99e8b89137ad7424ed49c60901
Vertic <thisistherealequinoctic@gmail.com>
2022-07-28 16:42:43 +0800

    Update FileWriter.cs

--- Revenge.RevLib/FileWriter.cs
+++ Revenge.RevLib/FileWriter.cs
@@ -2,14 +2,18 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Revenge.User;
 
 namespace Revenge.RevLib
 {
 	public class FileWriter
 	{
 		public string WriteFile(string? file, string? ext, string? contents) {
-			string _file = File.WriteAllText(@$"{file}.{ext}", contents);
-			return _file;
+			string _file = @$"{file}.{ext}";
+			string doc_path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			StreamWriter out_file = new StreamWriter(Path.Combine(doc_path, _file));
+			out_file.WriteLine(contents);
+			return Convert.ToString(out_file);
 		}
 	}
 }
