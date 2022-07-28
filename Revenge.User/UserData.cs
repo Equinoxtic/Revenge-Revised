cb50826e9c4b47d847a0ae441af99c8c9506234e
Vertic <thisistherealequinoctic@gmail.com>
2022-07-28 16:42:55 +0800

    Update UserData.cs

--- Revenge.User/UserData.cs
+++ Revenge.User/UserData.cs
@@ -2,14 +2,16 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Revenge.RevLib;
 
 namespace Revenge.User
 {
 	public class UserData
 	{
 		public static string GetHash(int hash_length) {
+			FileWriter fileWriter = new FileWriter();
 			string hash = UserHashes.CreateHash(hash_length);
-			string file = File.WriteAllText("userdat.txt", hash);
+			string file = fileWriter.WriteFile("userdat", "txt", hash);
 			return (!File.Exists(file)) ? file : "EMPTY_HASH";
 		}
 	}
