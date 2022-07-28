d54b70280c23fba791b02a56184641cb04c65129
Vertic <thisistherealequinoctic@gmail.com>
2022-07-28 16:43:27 +0800

    Update UserPass.cs

--- Revenge.User/UserPass.cs
+++ Revenge.User/UserPass.cs
@@ -2,11 +2,16 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Revenge.RevLib;
 
 namespace Revenge.User
 {
 	public class UserPass
 	{
-		// TODO: Finish code
+		public string GetUserPassword(string? pass) {
+			FileReader fileReader = new FileReader();
+			string pass_content = fileReader.ReadFile("pass", "txt");
+			return (!File.Exists(pass_content)) ? pass_content : "Password";
+		}
 	}
 }
