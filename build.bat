2d85154e19fb11ff633c836bac7b9c7aebbee272
Vertic <thisistherealequinoctic@gmail.com>
2022-09-02 21:11:09 +0800

    Update batch script files

--- build.bat
+++ build.bat
@@ -1,3 +1,1 @@
 dotnet build
-copy cursor.txt bin\Debug\net6.0\
-copy version.txt bin\Debug\net6.0\
--- run.bat
+++ run.bat
@@ -1,5 +1,3 @@
 dotnet run :: Auto Compile
-copy cursor.txt bin\Debug\net6.0\
-copy version.txt bin\Debug\net6.0\
 clear
 start bin/Debug/net6.0/Revenge.exe
