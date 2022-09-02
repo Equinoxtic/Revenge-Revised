44637cffb75288276d7adbd8a5bf8477e25c4b10
Vertic <thisistherealequinoctic@gmail.com>
2022-09-02 21:10:51 +0800

    Remove some user stuff

--- Revenge.User/UserHandler.cs
+++ Revenge.User/UserHandler.cs
@@ -6,6 +6,15 @@ namespace Revenge.User
 {
 	public class UserHandler
 	{
-		// TODO: Finish code and other modules related to this class
+		public string GetUsername() {
+			FileReader fileReader = new FileReader();
+			string user_name_file = fileReader.ReadFile("username", "txt");
+			return (!File.Exists(user_name_file)) ? user_name_file : "User";
+		}
+
+		public string SetUsername(string? uname_str) {
+			string? uname = uname_str;
+			return (!String.IsNullOrEmpty(uname)) ? uname : "User";
+		}
 	}
 }
